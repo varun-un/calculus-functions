298a8b52e887138d8bf34b4d30f77710b3224f6b
varun_u <77634835+astroameba@users.noreply.github.com>
2021-05-15 11:56:22 -0400

    euler's method functions added

--- calculus-functions.js
+++ calculus-functions.js
@@ -8,10 +8,24 @@
  * @param {number} initial.x The initial coordinate's x-value
  * @param {number} initial.y The initial coordinate's y-value
  * @param {number} targetX The x-value of the coordinate for which to approximate
- * @param {number} deltaX The step size to use for the approximation
+ * @param {number} deltaX The step size to use for the approximation (by default .01)
+ * @returns {number} The approximate y-value of the function at the given target x value
  */
 const euler1 = (differentialEq, initial, targetX, deltaX = .01) => {
 
+    deltaX = initial.x < targetX ? Math.abs(deltaX) : Math.abs(deltaX) * -1
+
+    var y = initial.y
+    for (var i = 1; i <= (targetX - initial.x) / deltaX; i++){
+        try {
+            y += differentialEq(initial.x + i * deltaX) * deltaX
+        }
+        catch (err) {
+            console.error('The differential equation function is undefined or not in the right format:' + err)
+        }
+    }
+
+    return y
 }
 
 /**
@@ -24,10 +38,23 @@ const euler1 = (differentialEq, initial, targetX, deltaX = .01) => {
  * @param {number} initial.x The initial coordinate's x-value
  * @param {number} initial.y The initial coordinate's y-value
  * @param {number} targetX The x-value of the coordinate for which to approximate
- * @param {number} deltaX The step size to use for the approximation
+ * @param {number} deltaX The step size to use for the approximation (by default .01)
+ * @returns {number} The approximate y-value of the function at the given target x value
  */
 const euler2 = (differentialEq, initial, targetX, deltaX = .01) => {
-    
+    deltaX = initial.x < targetX ? Math.abs(deltaX) : Math.abs(deltaX) * -1
+
+    var y = initial.y
+    for (var i = 1; i <= (targetX - initial.x) / deltaX; i++){
+        try {
+            y += differentialEq(initial.x + i * deltaX, y) * deltaX
+        }
+        catch (err) {
+            console.error('The differential equation function is undefined or not in the right format:' + err)
+        }
+    }
+
+    return y
 }
 
 
